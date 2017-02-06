691e98ec78203404aab61ea83667bb7a47150383
Vasily Korytov <vasily.korytov@icloud.com>
2017-02-06 03:51:45 +0200

    it seems like we can drop win32 (but keep win64)

--- examples/jsonmon.bat
+++ examples/jsonmon.bat
@@ -1,11 +1,15 @@
 @echo off
 rem jsonmon start-up script for Windows
-rem Can be used from Task Manager, NSSM and others
 
 rem HTTP host & port
 rem Set host to [::] to listen on all interfaces
 set HOST=localhost
 set PORT=3000
 
+rem Include sh.exe to PATH
+rem set PATH=C:\Program Files (x86)\Git\bin;%PATH%
+
+rem Directory holding jsonmon.exe and config.yml
 cd %SYSTEMDRIVE%\jsonmon
+
 jsonmon.exe -syslog config.yml
