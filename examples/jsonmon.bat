d670446988aead7e9300e63495cdca02a6f7886a
Vasily Korytov <vasily.korytov@icloud.com>
2017-02-06 04:51:02 +0200

    nicer windows example

--- examples/jsonmon.bat
+++ examples/jsonmon.bat
@@ -1,12 +1,13 @@
-@echo off
 rem jsonmon start-up script for Windows
+rem If you want to start jsonmon automatically, you can use Task Scheduler
+@echo off
 
 rem HTTP host & port
 rem Set host to [::] to listen on all interfaces
 set HOST=localhost
 set PORT=3000
 
-rem Include sh.exe to PATH
+rem Include `sh` in PATH
 rem set PATH=C:\Program Files (x86)\Git\bin;%PATH%
 
 rem Directory holding jsonmon.exe and config.yml
